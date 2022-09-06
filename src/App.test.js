25e8e611bdd27cbac62d89c0e03101a4b9530e53
dunnjacoba <101910318+dunnjacoba@users.noreply.github.com>
2022-09-06 12:02:42 -0500

    'adds restaurantData'

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Home from "./pages/home";
+import Restaurant from "./pages/restaurant";
 
-test("Click", () => {
+test("Home", () => {
   render(<Home />);
   const needHelp = screen.getByText(/Need help/i);
   expect(needHelp).toBeInTheDocument();
+});
+
+test("Restaurants", () => {
+  render(<Restaurant />);
+  const display = screen.getByText(/Selection/i);
+  expect(display).toBeInTheDocument();
 });
