01f815126fedf173dcf81044e8a412649295c393
dunnjacoba <101910318+dunnjacoba@users.noreply.github.com>
2022-09-06 12:03:01 -0500

    'Adds restaurantData'

--- src/components/restaurantData.js
+++ src/components/restaurantData.js
@@ -1,23 +1,21 @@
-export default function Restaurants() {
-  const restaurant = [
-    {
-      name: "McDonald's",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/180px-McDonald%27s_Golden_Arches.svg.png",
-      site: "https://www.mcdonalds.com/us/en-us.html",
-    },
-    {
-      name: "Zaxby's",
-      image:
-        "https://en.wikipedia.org/wiki/Zaxby%27s#/media/File:Zaxby's_Logo.png",
-      site: "https://www.zaxbys.com/",
-    },
-    {
-      name: "Chik-fil-A",
-      image:
-        "https://www.chick-fil-a.com/-/media/images/cfacom/default-images/chick-fil-a-logo-vector.ashx",
-      site: "https://www.chick-fil-a.com/?gclid=Cj0KCQjwjbyYBhCdARIsAArC6LJ292ehtEdRy6kLKPTekbfmLMu_9ga6RLiePE0u7MapWsXsUN64oTUaAsdSEALw_wcB",
-    },
-  ];
-  return restaurant;
-}
+const restaurantData = [
+  {
+    name: "McDonald's",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/180px-McDonald%27s_Golden_Arches.svg.png",
+    site: "https://www.mcdonalds.com/us/en-us.html",
+  },
+  {
+    name: "Zaxby's",
+    image: "https://www.zaxbys.com/assets/images/brand/logo.png",
+    site: "https://www.zaxbys.com/",
+  },
+  {
+    name: "Chik-fil-A",
+    image:
+      "https://www.chick-fil-a.com/-/media/images/cfacom/default-images/chick-fil-a-logo-vector.ashx",
+    site: "https://www.chick-fil-a.com/?gclid=Cj0KCQjwjbyYBhCdARIsAArC6LJ292ehtEdRy6kLKPTekbfmLMu_9ga6RLiePE0u7MapWsXsUN64oTUaAsdSEALw_wcB",
+  },
+];
+
+export default restaurantData;
--- src/pages/restaurant.jsx
+++ src/pages/restaurant.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Layout from "../components/styles/layout";
 import Cards from "../components/cards.jsx";
+import { CardGroup } from "react-bootstrap";
+import restaurant from "../components/restaurantData";
 
 export default function Restaurant() {
-  const rest = {
-    name: "McDonald's",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/180px-McDonald%27s_Golden_Arches.svg.png",
-    site: "https://www.mcdonalds.com/us/en-us.html",
+  const restSelector = () => {
+    let restSelected = Math.floor(Math.random() * 3);
+
+    return restSelected;
   };
+
   return (
     <Layout>
       <div>Selection</div>
-      <Cards rest={rest} />
+      <CardGroup className="p-3 m-2">
+        <Cards rest={restaurant[restSelector()]} />
+      </CardGroup>
     </Layout>
   );
 }
